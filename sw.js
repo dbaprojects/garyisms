d5618f444bfff0bea88e6c577e37a9059c675165
David Barkess <david.barkess@sap.com>
2026-06-25 08:47:17 +0800

    v2.3 - PWA support: manifest, service worker, install button with instructions

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'garyisms-v2.3';
+const ASSETS = [
+  './index.html',
+  './bg2.png',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
